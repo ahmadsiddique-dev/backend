a13cb4a25cd44a9786e0b42439199e0fc306e494
Ahmad Siddique <asshikrani66@gmail.com>
2025-09-13 03:51:14 +0500

    api handling in express

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,29 @@
+
+const asynchandler = (reqhandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqhandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+// const asynchandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status((error.code || 500).json({
+//             success : false,
+//             message : error.message
+//         }))
+//     }
+// }
+
+// const asynchandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.send((error.code || 500).json({
+//             success : false,
+//             message : error.message
+//         }))
+//     }
+// }
+// export asynchandler;
