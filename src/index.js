09d550b4d7a70d81ec0c4aab28d5f2ee40618595
Ahmad Siddique <asshikrani66@gmail.com>
2025-09-12 18:31:30 +0500

    cors working and its obj in node

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is live on ${process.env.PORT || 8000}`)
+  })
+})
+.catch((error) => {
+  console.log("MDB connection failed : ", error);
+})
 
 
 
